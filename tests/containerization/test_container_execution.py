e81f16ca83ab89f764b349e6b60e10bbbd4ac6e5
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-01-15 14:47:07 -0500

    Extrapolate Container Management in Test

--- tests/containerization/test_container_execution.py
+++ tests/containerization/test_container_execution.py
@@ -18,22 +18,70 @@ from tests.fixtures.utils import is_docker_present
 from tests.standard_tools.test_comparison import comparison_result_dict_test
 
 
+def build_image_and_run_experiment(input_dir: Path, output_dir: Path, input_file: Path):
+    experiment_deps = get_experiment_deps()
+    docker_image_path = f"{input_dir}{os.sep}Dockerfile"
+    docker_tag = "test_crbm_containerization"
+
+    with open(docker_image_path, "w") as f:
+        docker_file = formulate_dockerfile_for_necessary_env(
+            ContainerizationProgramArguments(
+                containerization_type=ContainerizationTypes.SINGLE,
+                containerization_engine=ContainerizationEngine.DOCKER,
+                working_directory=Path(input_dir),
+                input_file_path=str(input_file),
+            ),
+            experiment_deps,
+        )
+        f.write(docker_file.representation)
+
+    # Subprocess because SDK seems to have problems building containers for other platforms
+    subprocess.run(
+        [
+            "docker",
+            "buildx",
+            "build",
+            "--platform=linux/amd64",
+            "--load",
+            # "--no-cache",
+            "-t",
+            docker_tag,
+            str(input_dir),
+        ],
+        check=True,
+    )
+
+    # Bind dir with all related files to /experiment
+    client = docker.from_env()
+    client.containers.run(
+        image="test_crbm_containerization:latest",
+        remove=True,
+        volumes={
+            input_dir: {"bind": "/experiment/input", "mode": "rw"},
+            output_dir: {"bind": "/experiment/output", "mode": "rw"},
+        },
+        environment={
+            "PB_INPUT_FILE_PATH": f"/experiment/input/{input_file.name}",
+            "PB_OUTPUT_DIRECTORY": "/experiment/output",
+            "LOGGER_LEVEL": "DEBUG",
+            "PB_INTERVAL": 1,
+        },
+        platform="linux/amd64",
+        stderr=True,
+        stdout=True,
+    )
+
+
 @pytest.mark.skipif(not is_docker_present(), reason="docker daemon is not running")
 def test_execution_of_container(comparison_document: dict[Any, Any]) -> None:
-    client = docker.from_env()
-    experiment_deps = get_experiment_deps()
-
     with tempfile.TemporaryDirectory(delete=False) as tmpdir:
         input_dir = Path(tmpdir) / "input"
         output_dir = Path(tmpdir) / "output"
         os.mkdir(input_dir)
         os.mkdir(output_dir)
 
-        docker_image_path = f"{input_dir}{os.sep}Dockerfile"
-        docker_tag = "test_crbm_containerization"
         model_name = "model.xml"
-        comparison_pbg_name = "comparison.pbif"
-        comparison_pbg_path = f"{input_dir}{os.sep}{comparison_pbg_name}"
+        comparison_pbg_path = Path(f"{input_dir}{os.sep}comparison.pbif")
 
         shutil.copyfile(_get_model_path(), input_dir / model_name)
         with open(comparison_pbg_path, "w") as f:
@@ -41,54 +89,7 @@ def test_execution_of_container(comparison_document: dict[Any, Any]) -> None:
             comparison_doc_str = comparison_doc_str.replace(_get_model_path(), f"/experiment/input/{model_name}")
             f.write(comparison_doc_str)
 
-        with open(docker_image_path, "w") as f:
-            docker_file = formulate_dockerfile_for_necessary_env(
-                ContainerizationProgramArguments(
-                    containerization_type=ContainerizationTypes.SINGLE,
-                    containerization_engine=ContainerizationEngine.DOCKER,
-                    working_directory=Path(input_dir),
-                    input_file_path=comparison_pbg_path,
-                ),
-                experiment_deps,
-            )
-            print(docker_file.representation)
-            f.write(docker_file.representation)
-
-        # Subprocess because SDK seems to have problems building containers for other platforms
-        subprocess.run(
-            [
-                "docker",
-                "buildx",
-                "build",
-                "--platform=linux/amd64",
-                "--load",
-                # "--no-cache",
-                "-t",
-                docker_tag,
-                str(input_dir),
-            ],
-            check=True,
-        )
-
-        # Bind dir with all related files to /experiment
-        #
-        client.containers.run(
-            image="test_crbm_containerization:latest",
-            remove=True,
-            volumes={
-                input_dir: {"bind": "/experiment/input", "mode": "rw"},
-                output_dir: {"bind": "/experiment/output", "mode": "rw"},
-            },
-            environment={
-                "PB_INPUT_FILE_PATH": f"/experiment/input/{comparison_pbg_name}",
-                "PB_OUTPUT_DIRECTORY": "/experiment/output",
-                "LOGGER_LEVEL": "DEBUG",
-                "PB_INTERVAL": 1,
-            },
-            platform="linux/amd64",
-            stderr=True,
-            stdout=True,
-        )
+        build_image_and_run_experiment(input_dir, output_dir, comparison_pbg_path)
 
         result_file = next(k for k in os.listdir(output_dir) if ".pbg" in k)
         with open(os.path.join(output_dir, result_file)) as f:
